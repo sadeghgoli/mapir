ca314ecf39deb1d58003ee3e5415cd5942bbabdd
unknown <sadegh.goli1382@gmail.com>
2026-06-15 08:25:30 +0330

    back to sabzevar layer

--- app/components/map/layers/BaseTileLayer.tsx
+++ app/components/map/layers/BaseTileLayer.tsx
@@ -11,8 +11,8 @@ const TileLayer = dynamic(
 export default function BaseTileLayer() {
     return (
         <TileLayer
+            url="https://osm.sabzevar.ir:4443/tile/raster/{z}/{x}/{y}.png"
             // url="https://osm.sabzevar.ir:4443/tile/raster/{z}/{x}/{y}.png"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             maxZoom={19}
             minZoom={1}
