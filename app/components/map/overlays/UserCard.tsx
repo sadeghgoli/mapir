377661382d5d333c38bad0c269278f90e4cc8993
unknown <sadegh.goli1382@gmail.com>
2026-07-05 10:21:16 +0330

    change icon sizes

--- app/components/map/overlays/UserCard.tsx
+++ app/components/map/overlays/UserCard.tsx
@@ -52,8 +52,8 @@ export default function UserCard() {
     };
 
     return (
-        <div className="fixed top-22 md:top-8 left-8 z-[1000]">
-            <div className="flex items-center gap-3">
+        <div className="fixed top-22 md:top-8 left-6 z-[1000]">
+            <div className="flex items-center gap-2">
                 <div
                     onClick={handleClick}
                     className="bg-cyan-800 text-white rounded-lg px-2 h-12 shadow-xl flex items-center gap-4 cursor-pointer hover:bg-cyan-900 transition-colors"
@@ -67,10 +67,10 @@ export default function UserCard() {
                     </div>
                 </div>
 
-                <Link href="https://sabzevar.ir" target="_blank">
+                <Link href="https://sabzevar.ir" target="_blank" className=''>
                     <button
                         id="back-to-site"
-                        className="md:w-12 md:h-12 w-10 h-10 rounded-lg bg-white shadow-lg flex items-center justify-center hover:bg-gray-50 transition-colors"
+                        className="md:w-12 md:h-12 w-12 h-12 rounded-lg bg-white shadow-lg flex items-center justify-center hover:bg-gray-50 transition-colors"
                     >
                         <img src="/images/solar_round-arrow-left-bold-duotone.png" alt="بازگشت به سایت" />
                     </button>
